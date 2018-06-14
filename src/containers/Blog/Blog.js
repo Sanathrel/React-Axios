e8811995d256397f792970c2c2e5ead338cb9d1f
Sanathrel <Sanathrel@bk.ru>
2018-06-14 23:20:34 +0300

    First commit.

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+	state = {
+		posts: [],
+		selectedPostID: null,
+		error: false
+	}
+
+	postSelected = (id) => {
+		this.setState({ selectedPostID: id });
+	}
+
+    render () {
+		let posts = <p style={{ textAlign: 'center' }}>Something went wrong.</p>
+
+		if (!this.state.error) {
+			posts = this.state.posts.map( post => {
+				return <Post 
+					key={post.id} 
+					title={post.title} 
+					author={post.author}
+					clicked={() => this.postSelected(post.id)} />
+			});
+		}
+
+        return (
+            <div>
+                <section className="Posts">
+                	{posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostID} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+		axios.get('http://jsonplaceholder.typicode.com/posts')
+			.then(response => {
+				const posts = response.data.slice(0, 4);
+				const updatedPosts = posts.map(post => {
+					return {
+						...post,
+						author: 'Max'
+					}
+				})
+				this.setState({ posts: updatedPosts });
+			})
+			.catch(error => {
+				this.setState({ error: true });
+			});
+    }
+}
+
+export default Blog;
